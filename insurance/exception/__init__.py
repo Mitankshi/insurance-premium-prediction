cda77ff15aa375dd5f797f03b5fb7a2d7dcc4670
mitankshi <mitankshi143@gmail.com>
2023-03-04 15:51:27 +0530

    logger and exception file updated

---
+++ insurance/exception/__init__.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+class InsuranceException(Exception):
+
+    def __init__(self,error_message : Exception, error_detail : sys):
+        super().__init__(error_message)
+        self.error_message = InsuranceException.error_message_Detail(error_message,error_detail=error_detail)
+
+    @staticmethod
+    def error_message_Detail(error : Exception, error_detail : sys)->str:
+        _, _, exc_tb = error_detail.exc_info()
+        line_number = exc_tb.tb_frame.f_lineno
+
+        #extracting file name from exception traceback
+        file_name = exc_tb.tb_frame.f_code.co_filename
+
+        #preparing error message
+        error_message = f"error occurred python script name[{file_name}]" \
+                        f" line number [{exc_tb.tb_lineno}] error message [{error}]."
+        return error_message
+
+    def __Str__(self):
+        return InsuranceException.__name__.__str__()
+
+
+
---
+++ main.py
@@ -0,0 +1,20 @@
+from insurance.logger import logging
+from insurance.exception import InsuranceException
+import os,sys
+
+def test_logger_and_exception():
+    try:
+        logging.info("starting the test_logger_and_exception")
+        result = 3 / 0
+        print(result)
+        logging.info("ending point of the test_logger_and_exception")
+    except Exception as e:
+        logging.debug(str(e))
+        raise InsuranceException(e,sys)
+
+
+if __name__ == "__main__":
+    try:
+        test_logger_and_exception()
+    except Exception as e:
+        print(e)
