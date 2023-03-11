ecb213ab01526b1af61062ba1fd039fa122c4055
Mitankshi <mitankshi143@gmail.com>
2023-03-11 17:40:40 +0530

    updated data transformationfile

--- insurance/entity/config_entity.py
+++ insurance/entity/config_entity.py
@@ -6,6 +6,9 @@ from datetime import datetime
 FILE_NAME = "insurance.csv"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
+TRANSFORMER_OBJECT_FILE_NAME = "transformer.pkl"
+TARGET_ENCODER_OBJECT_FILE_NAME= "target_encoder.pkl"
+
 
 class TrainingPipelineConfig:
     def __init__(self):
@@ -42,3 +45,13 @@ class DataValidationConfig:
         self.report_file_path = os.path.join(self.data_validation_dir, "report.yaml") #yaml,json,csv
         self.base_file_path = os.path.join("insurance.csv")
         self.missing_threshold:float = 0.2
+
+
+class DataTransformationConfig:
+
+    def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir, "data_transformation")
+        self.transform_object_path = os.path.join(self.data_transformation_dir, "transformer", TRANSFORMER_OBJECT_FILE_NAME)
+        self.transform_train_path = os.path.join(self.data_transformation_dir, "transformed", TRAIN_FILE_NAME.replace("csv", "npz"))
+        self.transform_test_path = os.path.join(self.data_transformation_dir, "transformed", TEST_FILE_NAME.replace("csv", "npz"))
+        self.target_encoder_object_path = os.path.join(self.data_transformation_dir, "traget_encoder", TARGET_ENCODER_OBJECT_FILE_NAME)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from insurance.entity.config_entity import DataIngestionconfig
 from insurance.entity import config_entity
 from insurance.components.data_ingestion import DataIngestion
 from insurance.components.data_validation import DataValidation
+from insurance.components.data_transformation import DataTransformation
 
 #def test_logger_and_exception():
     #try:
@@ -38,6 +39,11 @@ if __name__ == "__main__":
 
            data_ingestion_artifact = data_validation.initiate_data_validation()
 
+           # data transformation
+
+           data_transformation_config= config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+           data_transformation= DataTransformation(data_transformation_config=data_transformation_config,data_ingestion_artifact= data_ingestion_artifact)
+           data_transformation_artifact = data_transformation.initiate_data_transformation()
     
 
         except Exception as e:
