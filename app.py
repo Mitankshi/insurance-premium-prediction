34359df2ea23fe3be8e7659f0b1c9fbf89e6d058
Mitankshi <mitankshi143@gmail.com>
2023-03-16 16:08:33 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+
 import numpy as np
 import pandas as pd
 
+import pickle
+
+
+model = pickle.load(open('model.pkl', 'rb'))
+encoder = pickle.load(open('target_encoder.pkl', 'rb'))
+transformer = pickle.load(open('transformer.pkl', 'rb'))
 
 st.title("Insurance Premium Prediction")
+
+Gender = st.selectbox("Please select your gender", ('male', 'female'))
